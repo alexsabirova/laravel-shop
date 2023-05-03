a29729ad8494c0ec67594eac23f570430386788b
alexsabirova <alexsabirova@gmmail.com>
2023-05-03 17:21:56 +0700

    - add migrations, factories, tables; - create slug Trait

---
+++ app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Slugable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+    use Slugable;
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'thumbnail',
+        'price',
+        'brand_id',
+    ];
+
+    public function slugSource(): string
+    {
+        return 'title';
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
